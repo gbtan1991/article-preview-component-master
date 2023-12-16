ba0a187adda76b2602b0ac217575adfe46828ad0
DelrosarioRovic <rovicdelrosario01@gmail.com>
2023-12-16 15:37:18 +0800

    fix share into 1 share component

--- src/component/Share.jsx
+++ src/component/Share.jsx
@@ -14,28 +14,24 @@ const Share = ({ toggleShare, showShare }) => {
   };
 
   return (
-    
     <div className="absolute right-0 sm:-right-28 top-0 sm:-top-20 h-[60px] w-full  sm:h-[50px] sm:w-[280px]">
       <div className=" px-8 bg-veryDarkGrayisBlue flex items-center justify-between rounded-br-xl rounded-bl-xl sm:rounded-tr-xl sm:rounded-tl-xl w-full h-full sm:absolute sm:w-full">
-        
-        <div className="flex flex-row gap-5 items-center">
+        <div className="flex flex-row gap-5 items-center md:justify-between w-full">
           <p className="text-lightGrayishBlue font-extralight font-Manrope text-sm ">
             S H A R E
           </p>
-          <FaFacebookSquare color="#fff" size={20} className="cursor-pointer" />
-          <FaTwitter color="#fff" size={20} className="cursor-pointer" />
-          <FaPinterest color="#fff" size={20} className="cursor-pointer" />
+          <div className="flex gap-5">
+            <FaFacebookSquare
+              color="#fff"
+              size={20}
+              className="cursor-pointer"
+            />
+            <FaTwitter color="#fff" size={20} className="cursor-pointer" />
+            <FaPinterest color="#fff" size={20} className="cursor-pointer" />
+          </div>
         </div>
-        <button
-          className='bg-desaturatedDarkBlue w-8 h-8 rounded-full flex items-center justify-center'
-          onClick={toggleShare}
-        >
-          <FaShare color="#fff" />
-        </button>
       </div>
-      </div>
-
-    
+    </div>
   );
 };
 
