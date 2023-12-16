8fe74d273edf3a5421f3a4a67e56c5e4489582a7
Gilbert Tan <57788135+gbtan1991@users.noreply.github.com>
2023-12-16 11:51:27 +0800

    Add the conditional rendering

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import avatar from "../assets/images/avatar-michelle.jpg";
 import shareBlk from "../assets/images/share-solid.svg";
 import Share from './Share'
+import { FaShare } from "react-icons/fa";
 
 const Card = () => {
     const [ showShare, setShowShare ] =  useState(false)
@@ -12,21 +13,21 @@ const Card = () => {
 
   return (
     <section className="grid place-content-center h-screen shadow-lg">
-      <div className="flex flex-col w-[330px] sm:flex-row sm:w-[730px] sm:h-[280px]">
+      <div className="flex flex-col  w-[330px] sm:flex-row sm:w-[730px] sm:h-[280px]">
         <div className="h-[220px] w-full bg-header-drawer bg-cover bg-top rounded-tr-xl rounded-tl-xl rounded-bt sm:h-full sm:w-full sm:bg-left-top sm:rounded-tr-none sm:rounded-bl-xl"></div>
 
-        <div className="bg-white px-8 py-5 rounded-br-xl rounded-bl-xl sm:px-10 sm:py-6 sm:rounded-bl-none sm:rounded-tr-xl ">
-          <h2 className="py-3 font-Manrope text-veryDarkGrayisBlue font-extrabold sm:text-xl">
+        <div className="bg-white px-0 py-5 justify-between rounded-br-xl rounded-bl-xl sm:px-10 sm:py-6 sm:rounded-bl-none sm:rounded-tr-xl ">
+          <h2 className="py-3 px-8 font-Manrope text-veryDarkGrayisBlue font-extrabold sm:text-xl">
             Shift the overall look and feel by adding these wonderful touches to
             furniture in your home
           </h2>
-          <p className="text-sm text-desaturatedDarkBlue">
+          <p className="text-sm text-desaturatedDarkBlue px-8 ">
             Ever been in a room and felt like something was missing? Perhaps it
             felt slightly bare and uninviting. I've got some simple tips to help
             you make any room feel complete.
           </p>
           <div>
-            <div className="flex flex-row justify-between items-center pt-5">
+           {showShare ? <Share toggleShare={toggleShare}/> : (<div className="flex flex-row   px-8">
               <div className="flex flex-row items-center gap-3 ">
                 <img
                   src={avatar}
@@ -43,15 +44,9 @@ const Card = () => {
                 </div>
               </div>
               <button className="bg-lightGrayishBlue w-8 h-8 rounded-full flex items-center justify-center" onClick={toggleShare}>
-                <img
-                  src={shareBlk}
-                  alt="Share icon"
-                  className="opacity-75 self-center"
-                />
+                <FaShare color="" />
               </button>
-            </div>
-
-            {showShare && <Share toggleShare={toggleShare} />}
+            </div>)}
             
           </div>
         </div>
--- src/component/Share.jsx
+++ src/component/Share.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import { FaFacebook, FaFacebookF, FaFacebookSquare, FaPinterest, FaShare, FaTwitter } from "react-icons/fa";
 
 
 const Share = ({toggleShare}) => {
   return (
-    <div className=''>
-        SHARE facebook twitter pinterest 
-        <button className="bg-lightGrayishBlue w-8 h-8 rounded-full flex items-center justify-center" onClick={toggleShare}>
-                <img
-                  // src={shareBlk}
-                  alt="Share icon"
-                  className="opacity-75 self-center"
-                />
+    <div className=' px-8 bg-veryDarkGrayisBlue flex items-center justify-around  rounded-br-xl rounded-bl-xl w-full h-[65px]'>
+        <div className='flex flex-row gap-5 items-center'>
+        <p className='text-lightGrayishBlue font-extralight font-Manrope text-sm '>S H A R E</p> 
+        <FaFacebookSquare color='#fff' size={23}/>
+        <FaTwitter color='#fff' size={23}/>        
+        <FaPinterest color='#fff' size={23}/>
+        </div>
+        <button className="bg-desaturatedDarkBlue w-8 h-8 rounded-full flex items-center justify-center" onClick={toggleShare}>
+        <FaShare  color='#fff'/>
               </button>
     </div>
   )
